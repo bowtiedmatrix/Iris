79b029ef422a4df5c27ea28e6c322b72002ce533
nc18860 <nc18860@essex.ac.uk>
2021-12-31 18:32:43 +0000

    Added classification report to show precision, recall, f1-score

--- ProjectIris/IrisClassification.py
+++ ProjectIris/IrisClassification.py
@@ -6,6 +6,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report
 
 # Load the dataset
 
@@ -55,4 +56,6 @@ model.fit(x_train, y_train)
 
 yhat =  model.predict(x_test)
 accuracy = accuracy_score(y_test, yhat)
+
+print(classification_report(y_test, yhat))
 print('Accuracy: %.3f' % accuracy)
