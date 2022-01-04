a6344dfe5f115685879d3d420011bad1cef4dee1
nc18860 <nc18860@essex.ac.uk>
2022-01-04 17:43:28 +0000

    Implemented a Decision Tree solution for classification of the Iris dataset

--- ProjectIris/IrisClassificationDT.py
+++ ProjectIris/IrisClassificationDT.py
@@ -1,12 +1,11 @@
 import pandas as pd
-import numpy as np
 from collections import Counter
-from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
-
 
 # Load the dataset
 
@@ -24,13 +23,15 @@ df_shape = data.shape
 # We want to classify by 'Species' (3 distinct values)
 # Multi-class classification
 
+# summarise observations by class label
+# WHAT DOES COUNTER DO?
 counter = Counter(data['Species'])
 
 # define the dataset
 x = data.drop(['Species', 'PetalLengthCm', 'PetalWidthCm'], axis=1)
 y = data.Species
 
-# SVC for multi-class classification using built-in one-vs-one
+# decision tree for multi-class classification
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=1)
 scale = StandardScaler()
@@ -38,16 +39,19 @@ x_train = scale.fit_transform(x_train)
 x_test = scale.transform(x_test)
 
 # define the model
-
-# WHAT DOES 'decision_function_shape' DO?
-model = SVC(decision_function_shape='ovo')
+# HOW DOES THIS WORK INTERNALLY?
+model = DecisionTreeClassifier(max_depth=2)
 
 # fit the model
 model.fit(x_train, y_train)
 
 yhat = model.predict(x_test)
+
+# create a confusion matrix
+cm = confusion_matrix(y_test, yhat)
+print(cm)
+
 accuracy = accuracy_score(y_test, yhat)
 
 print(classification_report(y_test, yhat))
 print('Accuracy: %.3f' % accuracy)
-
